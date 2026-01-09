bc8411cabf8bc6798b581ad8521d2d47c4f79ce2
Yordan <yordan@soltein.mx>
2026-01-09 12:49:45 -0600

    [IMP] Update subscription link text for clarity; increment version number to 18.0.1.1.1

--- solt_tiendanube/__manifest__.py
+++ solt_tiendanube/__manifest__.py
@@ -10,7 +10,7 @@
     'website': 'https://www.soltein.mx',
     'support': 'soporte@soltein.mx',
     'maintainers': ['soltein'],
-    'version': '18.0.1.1.0',
+    'version': '18.0.1.1.1',
     'license': 'LGPL-3',
     'category': 'Sales/Multichannel',
     'application': True,
