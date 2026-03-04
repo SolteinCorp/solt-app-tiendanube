34df3816733d777b52375ea815c319c78016e515
Yosbanis Vicente <90yobi90@gmail.com>
2026-03-04 12:06:44 -0500

    [FIX] __manifest__.py: Update version to 19.0.1.0.3 and adjust group tag in solt_api_call_log.xml

--- solt_api_connector/__manifest__.py
+++ solt_api_connector/__manifest__.py
@@ -16,7 +16,7 @@ webhook support.
     'website': 'https://www.soltein.mx',
     'support': 'soporte@soltein.mx',
     'category': 'Tools/Connectivity',
-    'version': '19.0.1.0.2',
+    'version': '19.0.1.0.3',
     'license': 'LGPL-3',
     'depends': ['base', 'base_automation', 'social_media'],
     'application': True,
