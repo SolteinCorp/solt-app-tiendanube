40f810898e0eed96432e53f349ebcba901b10027
Yosbanis Vicente <90yobi90@gmail.com>
2026-04-12 18:43:54 -0500

    Revert "[ADD] Implement safe_eval patch for Python 3.12+ opcodes and update model fold_name retrieval"
    This reverts commit 52ca496b282f2185700564e6d361b6bf569c0f23.

--- solt_api_connector/__init__.py
+++ solt_api_connector/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 # Copyright 2024 Soltein SA. de CV.
 # License LGPL-3 or later (http://www.gnu.org/licenses/lgpl.html)
-from . import patches  # noqa: F401 - aplica parches sobre safe_eval al cargar el módulo
 from . import models
