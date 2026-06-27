57da319dadf4290132b08ebbb8ae1559f9d59f83
Yordan <yordan@soltein.mx>
2026-06-26 23:04:33 -0600

    [I18N] solt_api_connector: es_MX translations for advanced config and install button

--- solt_api_connector/__manifest__.py
+++ solt_api_connector/__manifest__.py
@@ -9,7 +9,7 @@
     'website': 'https://www.soltein.mx',
     'support': 'soporte@soltein.mx',
     'category': 'Tools/Connectivity',
-    'version': '17.0.1.1.1',
+    'version': '17.0.1.1.2',
     'license': 'LGPL-3',
     'depends': ['base', 'base_automation', 'social_media'],
     'application': True,
